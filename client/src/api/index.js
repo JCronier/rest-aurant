51dbaa814967b8493d5c2f62869a9e79f239b980
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-22 20:43:24 -0800

    Change server port to 3001

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 // Items
-const items_url = 'http://localhost:3000/items';
+const items_url = `http://localhost:3001/items`;
 
 export const fetchItems = () => axios.get(items_url);
 
 // Orders
-const orders_url = 'http://localhost:3000/orders';
+const orders_url = 'http://localhost:3001/orders';
 
 export const fetchOrders = () => axios.get(orders_url);
 
 // Tables
-const tables_url = 'http://localhost:3000/tables';
+const tables_url = 'http://localhost:3001/tables';
 
 export const fetchTables = () => axios.get(tables_url);
