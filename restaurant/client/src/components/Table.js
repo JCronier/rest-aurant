7d17086fd9e7a349728ae8125cae45f6a4937022
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-24 15:33:41 -0800

    Add img tag for qr_code url

--- restaurant/client/src/components/Table.js
+++ restaurant/client/src/components/Table.js
@@ -8,6 +8,14 @@ const Table = ({ table }) => {
       <div>_id: {table._id}</div>
       <div>_d: {table.id}</div>
       <div>qr_code: {table.qr_code}</div>
+      <div>
+        <div>
+          qr_code image (NOT PART OF SCHEMA):
+        </div>
+        <div>
+          <img src={table.qr_code} alt="qr_code" height="100" width="100" />
+        </div>
+      </div>
       <div>status: {table.status}</div>
       <br />
     </div>
