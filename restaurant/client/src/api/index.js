9f8ad9b821fb44ad9c7162c5fd74c795967c59fd
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-24 14:07:11 -0800

    Setup initial HTTP requests for items, orders, and tables

---
+++ restaurant/client/src/api/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+// Items
+const items_url = `http://localhost:3001/items`;
+
+export const fetchItems = () => axios.get(items_url);
+
+// Orders
+const orders_url = `http://localhost:3001/orders`;
+
+export const fetchOrders = () => axios.get(orders_url);
+
+// Tables
+const tables_url = `http://localhost:3001/tables`;
+
+export const fetchTables = () => axios.get(tables_url);
