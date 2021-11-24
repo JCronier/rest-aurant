ace73115eaca1d084a46d6217d8a19e662b14611
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-24 14:25:55 -0800

    Implement get action for items, orders, and tables

---
+++ restaurant/client/src/actions/orders.js
@@ -0,0 +1,12 @@
+import * as api from '../api';
+
+// Action Creators
+export const getOrders = () => async (dispatch) => {
+  try {
+    const { data } = api.fetchOrders();
+
+    dispatch({ type: 'ORDERS/FETCH_ALL', payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
