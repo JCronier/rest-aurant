b89afad8b76d16860137d3f2bbb35acdf82bd271
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-24 12:57:59 -0800

    Only allow routes for RUD operations

--- restaurant/server/routes/orders.js
+++ restaurant/server/routes/orders.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 // Handlers
-import { getOrders, updateOrder } from '../controllers/orders.js';
+import { getOrders, updateOrder, deleteOrder } from '../controllers/orders.js';
 
 // Create New Express Router
 const router = express.Router();
@@ -9,5 +9,6 @@ const router = express.Router();
 // Routes
 router.get('/', getOrders);
 router.patch('/:id', updateOrder);
+router.delete('/:id', deleteOrder);
 
 export default router;
