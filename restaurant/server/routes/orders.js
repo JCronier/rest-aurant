9a62b2971b9adf6abc7cb2939ca8e38294c86a67
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-24 12:03:25 -0800

    Remove code for POST route

--- restaurant/server/routes/orders.js
+++ restaurant/server/routes/orders.js
@@ -1,14 +1,13 @@
 import express from 'express';
 
 // Handlers
-import { getOrders, createOrder, updateOrder } from '../controllers/orders.js';
+import { getOrders, updateOrder } from '../controllers/orders.js';
 
 // Create New Express Router
 const router = express.Router();
 
 // Routes
 router.get('/', getOrders);
-router.post('/', createOrder);
 router.patch('/:id', updateOrder);
 
 export default router;
