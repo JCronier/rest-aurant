a27b9611f6c752b346b76b823bfcbdd99f9eaaf5
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-24 11:50:39 -0800

    Create Order model for MongoDB

---
+++ restaurant/server/models/order.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const orderSchema = mongoose.Schema({
+  id: Number,
+  table: {
+    type: Number,
+    field: 'id',
+    ref: 'Table'
+  },
+  items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }],
+  isPaid: {
+    type: Boolean,
+    default: false
+  },
+  options: [Object]
+}, { minimize: falsse });
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
