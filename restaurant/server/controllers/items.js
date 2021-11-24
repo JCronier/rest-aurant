31b7fe502ef1f47558b503efebf0585ce7ba67b4
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-24 12:12:22 -0800

    Implement controllers for PATCH and DELETE requests for items

--- restaurant/server/controllers/items.js
+++ restaurant/server/controllers/items.js
@@ -1,3 +1,4 @@
+import { Mongoose } from 'mongoose';
 import Item from '../models/item.js';
 
 export const getItems = async (req, res) => {
@@ -25,9 +26,26 @@ export const createItem = async (req, res) => {
 };
 
 export const updateItem = async (req, res) => {
+  const { id: _id } = req.params;
+  const item = req.body;
 
+  if (!Mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send('No item with that id');
+  }
+
+  const updatedItem = await Item.findByIdAndUpdate(_id, { ...item, _id }, { new: true });
+
+  res.json(updatedItem);
 };
 
 export const deleteItem = async (req, res) => {
+  const { id: _id } = req.params;
 
+  if (!Mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send('No item with that id');
+  }
+
+  await Item.findByIdAndRemove(_id);
+
+  res.json({ message: 'Item deleted successfully' });
 };
