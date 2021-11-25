afe7af259406b693608245a0c0e30d28ebfd2e3b
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-24 19:34:54 -0800

    Set up acton to update an item

--- restaurant/client/src/actions/items.js
+++ restaurant/client/src/actions/items.js
@@ -20,3 +20,13 @@ export const createItem = (item) => async (dispatch) => {
     console.log(error.message);
   }
 };
+
+export const updateItem = (_id, item) => async (dispatch) => {
+  try {
+    const { data } = await api.updateItem(_id, item);
+
+    dispatch({ type: 'ITEMS/UPDATE', payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
