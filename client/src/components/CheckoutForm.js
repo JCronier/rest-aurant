520288b02b80989274b4a5956c81244f9374da32
Alejandro Cristoffanini <acristoffanini@gmail.com>
2021-11-25 17:54:27 -0800

    state handling implementation, sub total plus tip percentage implementation, selectable tip states implementation

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 
+//Table status API patch request
 import { updateTableStatus } from "../api";
+//Order context
 import { orderContext } from "../providers/OrderProvider";
 
 //Stripe API
@@ -22,7 +24,18 @@ const CheckoutForm = (props) => {
   //Local states for handling payment processing
   //Local state for handling the payment secret
   const [secret, setSecret] = useState(null);
-  const [checkoutState, setCheckoutState] = useState('INACTIVE')
+  // const [checkoutState, setCheckoutState] = useState('INACTIVE')
+  const [succeeded, setSucceeded] = useState(false);
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  // const [clientSecret, setClientSecret] = useState("");
+
+  const handleChange = async (event) => {
+    // 4️⃣ Listen for changes in the CardElement and display any errors as the customer types their card details
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
 
   //Request for payment intent from the API
   const handleSubmit = async (ev) => {
@@ -35,20 +48,19 @@ const CheckoutForm = (props) => {
       },
     });
     if (payload.error) {
-      // setError(`Payment failed ${payload.error.message}`);
-      // setProcessing(false);
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
       console.log('error occured: ',payload)
     } else {
-      // setError(null);
-      // setProcessing(false);
-      // setSucceeded(true);
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
       console.log('succesful payment: ',payload)
+      //uses the built-in API function to send a patch request to our table
       updateTableStatus(state.table, "PAID")
       console.log(`table ${state.table} changed to PAID`)
     }
   };
-
-  // export const updateTableStatus = (id, status) => axios.patch(tables_url + `/${id}`, { status })
 
   //Request to the server for payment secret
   useEffect(() => {
@@ -61,12 +73,24 @@ const CheckoutForm = (props) => {
       console.log(secret)
     )
     
-  }, [])
+  }, [props.amount])
+
+  // const options = 'bluh'
   
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement/>
-      <button>Submit</button>
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <CardElement id="card-element" onChange={handleChange}/>
+      <button disabled={processing || disabled || succeeded} id="submit">
+        <span id="button-text">
+          {processing ? <div className="spinner" id="spinner"></div> : "Pay"}
+        </span>
+      </button>
+      {/* Show any error that happens when processing the payment */}
+      {error && (
+        <div className="card-error" role="alert">{error}</div>
+      )}
+      {/* Show a success message upon completion */}
+      <p className={succeeded ? "result-message" : "result-message hidden"}>Payment succeeded!</p>
     </form>
     
   )
