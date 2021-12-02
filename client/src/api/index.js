22b3b12fff217a76399ca5987a542f086ebfa216
fr3chtoa45t <jcronier777@gmail.com>
2021-12-02 11:41:25 -0800

    sadd

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 const api_url = process.env.REACT_APP_NGROK_URL
 
 // Items
-const items_url = `${api_url}/items`;
+const items_url = `${api_url}items`;
 
 export const fetchItems = () => axios.get(items_url);
 
 // Orders
-const orders_url = `${api_url}/orders`;
+const orders_url = `${api_url}orders`;
 
 export const fetchOrders = () => axios.get(orders_url);
 export const createOrder = (newOrder) => axios.post(orders_url, newOrder);
@@ -16,14 +16,14 @@ export const updateOrderPaymentStatus = (id, isPaid) => axios.patch(orders_url +
 export const putOrder = (newOrder) => axios.put(orders_url + `/${newOrder.id}`, newOrder);
 
 // Tables
-const tables_url = `${api_url}/tables`;
+const tables_url = `${api_url}tables`;
 
 export const fetchTables = () => axios.get(tables_url);
 export const createTable = (newTable) => axios.post(tables_url, newTable);
 export const updateTableStatus = (id, status) => axios.patch(tables_url + `/${id}`, { status });
 
 // Receipts
-const receipts_url = `${api_url}/receipts`;
+const receipts_url = `${api_url}receipts`;
 
 export const fetchReceipts = () => axios.get(receipts_url);
 export const createReceipt = (newReceipt) => axios.post(receipts_url, newReceipt);
--- restaurant/client/src/api/index.js
+++ restaurant/client/src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api_url = process.env.REACT_APP_NGROK_URL
 
 // Items
-const items_url = `${api_url}/items`;
+const items_url = `${api_url}items`;
 
 export const fetchItems = () => axios.get(items_url);
 export const createItem = (newItem) => axios.post(items_url, newItem);
@@ -11,12 +11,12 @@ export const updateItem = (_id, updatedItem) => axios.patch(`${items_url}/${_id}
 export const deleteItem = (_id) => axios.delete(`${items_url}/${_id}`);
 
 // Orders
-const orders_url = `${api_url}/orders`;
+const orders_url = `${api_url}orders`;
 
 export const fetchOrders = () => axios.get(orders_url);
 
 // Tables
-const tables_url = `${api_url}/tables`;
+const tables_url = `${api_url}tables`;
 
 export const fetchTables = () => axios.get(tables_url);
 export const createTable = (newTable) => axios.post(tables_url, newTable);
